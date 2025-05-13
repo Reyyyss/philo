bf618aea7496d1255f43da01671d9c0b005ec977
Reyyyss <hcr2207@gmail.com>
2025-05-13 17:50:22 +0100

    inicio

---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 16:42:44 by hcarrasq          #+#    #+#             */
+/*   Updated: 2025/05/13 16:50:41 by hcarrasq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char **argv)
+{
+	if (argc < 4 || argc >5)
+		return (ft_printf("Error: Expected 4 or 5 arguments\n"), 1);
+	//buscar o ft_valid do push swap porque todos os argumentos precisam de
+	//ser numeros e precisam todos de ser maiores que 0 menos o ultimo 
+	//pois e opcional, posso tambem colocar um limite de filosofos para 200 
+	//porque diz que nao e suposto testar
+	
+}
