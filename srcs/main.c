a0779c59b18cfdb4e263c4ea9784093d1fd3c2c0
Reyyyss <hcr2207@gmail.com>
2025-05-15 17:17:46 +0100

    filosofando

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:42:44 by hcarrasq          #+#    #+#             */
-/*   Updated: 2025/05/14 17:38:47 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2025/05/15 17:07:27 by hcarrasq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	(void)argv;
 	if (argc < 4 || argc >5)
 		return (printf("Error: Expected 4 or 5 arguments\n"), 1);
-	//buscar o ft_valid do push swap porque todos os argumentos precisam de
-	//ser numeros e precisam todos de ser maiores que 0 menos o ultimo 
-	//pois e opcional, posso tambem colocar um limite de filosofos para 200 
-	//porque diz que nao e suposto testar
+	if ()
+
 	
 }
+
+t_philo	oh
